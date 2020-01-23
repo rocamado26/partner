8868f52ddff88bfb5366a083cefbaee8d7184be7
Roberto Martinez <rocamado26@gmail.com>
2020-01-23 14:56:23 -0600

    Cambiando mensaje

--- partner/partner/App.xaml.cs
+++ partner/partner/App.xaml.cs
@@ -1,4 +1,7 @@
-﻿using partner.Views.Reproductor;
+﻿using Microsoft.AppCenter;
+using Microsoft.AppCenter.Analytics;
+using Microsoft.AppCenter.Crashes;
+using partner.Views.Reproductor;
 using System;
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -16,6 +19,8 @@ namespace partner
 
         protected override void OnStart()
         {
+            AppCenter.Start("android=28311123-021c-4d97-ae31-22e3e8362d8e; ios=3087c8ab-a949-44e3-834f-8154e19a55d7",
+                  typeof(Analytics), typeof(Crashes));
         }
 
         protected override void OnSleep()
